1cd9aab0f8b6194830c1062ad228fd44095c7989
DESKTOP-L92N3UM\DenisPC <dselcan@gmail.com>
2017-11-27 20:19:45 +0100

    Dodal Google sign in

---
+++ app/src/main/java/com/example/denis/gorovje/GoogleSignInActivity.java
@@ -0,0 +1,111 @@
+package com.example.denis.gorovje;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class GoogleSignInActivity extends AppCompatActivity {
+
+    GoogleApiClient mGoogleApiClient;
+    Context context;
+    SignInButton button;
+    private static final int RC_SIGN_IN = 9001;
+    TextView mStatusTextView;
+    ApplicationMy am;
+    GoogleSignInResult result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_sign_in);
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        am = (ApplicationMy)getApplication();
+        context = this.getApplicationContext();
+        button = (SignInButton) findViewById(R.id.sign_in_button);
+        mStatusTextView = (TextView) findViewById(R.id.textView15);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                        Toast.makeText(context, "POVEZAVA NEUSPEŠNA", Toast.LENGTH_SHORT).show();
+                    }
+
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+            }
+        });
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(result != null && am.signedIn){
+            if(result.isSuccess()){
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setCancelable(false);
+                builder.setMessage("Izhod iz aplikacije?").setPositiveButton("ja", dialogClickListener)
+                        .setNegativeButton("ne", dialogClickListener).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_SIGN_IN){
+            result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            am.userEmail = acct.getEmail();
+            Intent intent = new Intent(this, TabbedActivity.class);
+            startActivity(intent);
+        } else {
+            // Signed out, show unauthenticated UI.
+            Toast.makeText(this, "LOGIN FAILED", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            switch (which){
+                case DialogInterface.BUTTON_POSITIVE:
+                    finish();
+                    System.exit(0);
+                    break;
+
+                case DialogInterface.BUTTON_NEGATIVE:
+                    Intent intent = new Intent(context, TabbedActivity.class);
+                    startActivity(intent);
+                    break;
+            }
+        }
+    };
+}
